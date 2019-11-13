2268f3221da2f7f13869e4a3aec5840b34440ff8
Christian Allred <christianallred@gmail.com>
2019-11-12 21:07:02 -0700

    Updates

---
+++ precache-manifest.42a96044677ba4cada3c7044fe1bc16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4bc4d710404d9227592c48b18aee3d",
+    "url": "/ffxiv-overlay/index.html"
+  },
+  {
+    "revision": "2def348d75408dbcdd24",
+    "url": "/ffxiv-overlay/static/css/main.032cb47b.chunk.css"
+  },
+  {
+    "revision": "35270c636e602a6cbe5e",
+    "url": "/ffxiv-overlay/static/js/2.7603a726.chunk.js"
+  },
+  {
+    "revision": "2def348d75408dbcdd24",
+    "url": "/ffxiv-overlay/static/js/main.0a8fe8f9.chunk.js"
+  },
+  {
+    "revision": "fe455779dc1b7fe8a139",
+    "url": "/ffxiv-overlay/static/js/runtime-main.68981185.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ffxiv-overlay/static/media/logo.25bf045c.svg"
+  }
+]);
